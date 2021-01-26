45e2139cba4ccfd2875b39d2a2ef59d2b34b896d
arshiya <arshiyarezaie@gmail.com>
2021-01-26 16:26:32 +0330

    DPID added to Switch.java

--- src/main/java/Network/Switch.java
+++ src/main/java/Network/Switch.java
@@ -5,17 +5,20 @@ import java.net.Inet4Address;
 public class Switch extends Node {
 
     private final Integer switchId;
+    private final String DPId;
     private static Integer firstAvailableSwitchId = 1;
 
-    public Switch(Inet4Address ip, String name) {
+    public Switch(Inet4Address ip, String name,String DPId) {
         super(ip, name);
         this.switchId = firstAvailableSwitchId;
+        this.DPId = DPId;
         firstAvailableSwitchId+=1;
     }
 
-    public Switch(Inet4Address ip) {
+    public Switch(Inet4Address ip,String DPId) {
         super(ip,"s"+firstAvailableSwitchId);
         this.switchId = firstAvailableSwitchId;
+        this.DPId = DPId;
         firstAvailableSwitchId+=1;
     }
 
